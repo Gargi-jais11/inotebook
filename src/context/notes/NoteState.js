28161593a3ba4a0e63fee399adc7eb8b858259d3
Gargi Jaiswal <134698352+Gargi-jais11@users.noreply.github.com>
2023-12-25 20:45:39 +0530

    Update NoteState.js

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,7 +2,7 @@ import noteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState=(props)=>{
-  const host="http://localhost:5000"
+  const host="https://inotebook-rsie.vercel.app"
     const notesInitial=[]
     const [notes,setNotes]=useState(notesInitial)
     
