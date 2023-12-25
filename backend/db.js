ad9a949b5f0b2a694ff2cc64316fc2918f982144
Gargi Jaiswal <134698352+Gargi-jais11@users.noreply.github.com>
2023-12-25 13:41:10 +0530

    All complete

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+const mongoose=require('mongoose');
+require('dotenv').config();
+//const mongooseURI="mongodb://127.0.0.1:27017/gargidataBase1";
+//console.log(mongooseURI);
+
+const connectToMongo=()=>{
+    mongoose.connect("mongodb://127.0.0.1:27017/inotebookDb")
+    .then(()=>{
+        console.log("connected to mongodb server");
+    })
+    .catch((error)=>{
+        console.error("Error connecting to MongoDb:",error.message)
+    });
+}
+module.exports=connectToMongo;
